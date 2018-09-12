50e9a8b390159fb4a7e408e1a55dbe99bed15b15
stla <laurent_step@yahoo.fr>
2018-09-12 12:28:13 +0200

    KM41

---
+++ R/datasets.R
@@ -0,0 +1,13 @@
+#' @title Krishnamoorthy & Mathew's example 4.1
+#' @description The dataset used in Krishnammorthy & Mathew's example 4.1.
+#' @name KM41
+#' @docType data
+#' @references Krishnamoorthy and Mathew, Statistical Tolerance Regions, Wiley 2009.
+#' @keywords data
+#' @usage data(KM41)
+#' @format A data frame with 25 rows and 2 columns.
+#' @examples
+#' data(KM41)
+#' str(KM41)
+#' table(KM41$Batch)
+NULL
